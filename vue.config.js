b57bff68902360fb7133324786f4ad9813c83110
lizx <lizx@nbport.com.cn>
2022-08-03 16:27:04 +0800

    端口配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8000,
+    proxy: {
+      '/user': {
+        target: 'http://127.0.0.1:8500',
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/user': ''
+        }
+      }
+    }
+  }
 })
